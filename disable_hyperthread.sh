219574677c2d6fb2f2e2f48a0fae5c24d246def7
INESC-machine <leila.sharifi@gmail.com>
2016-07-21 12:20:37 +0100

    disable/enable hyperthread updated

--- disable_hyperthread.sh
+++ disable_hyperthread.sh
@@ -2,7 +2,7 @@
 num_cores=$(cat /proc/cpuinfo | grep processor | wc -l)
 echo "number of cores="$num_cores
 
-for i in {4..7..1}
+for (( i=$((num_cores/2)); i < $num_cores ; i++ ))
 do
 	echo 0 > /sys/devices/system/cpu/cpu$i/online
 	echo "Thread "$i" is disabled"
--- enable_hyperthread.sh
+++ enable_hyperthread.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 num_cores=$(cat /proc/cpuinfo | grep processor | wc -l)
 echo "number of cores="$num_cores
-
-for i in {16..31..1}
+for (( i=$((num_cores/2)); i < $num_cores ; i++ ))
 do
-	echo 1 > /sys/devices/system/cpu/cpu$i/online
+	sudo echo 1 > /sys/devices/system/cpu/cpu$i/online
 	echo "Thread "$i" is enabled"
+	echo $i
 done
